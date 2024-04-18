74fb974b35bb7c13bdf6a2d854509e48bb071d40
Kim Ji Yu <spbabo97@naver.com>
2024-04-18 16:10:00 +0900

    :tada: 프로젝트 시작

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "구구모",
+  description: "동네 구기종목 매치 서비스 구구모 입니다.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      프로젝트 시작
+    </main>
+  );
+}
