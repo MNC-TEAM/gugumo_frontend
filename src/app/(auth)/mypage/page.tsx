63e7e5b1bb61004ca57e18a920b6b4767e36ff27
Kim Ji Yu <spbabo97@naver.com>
2024-05-13 05:43:10 +0900

    :bug: 회원가입시 발생하는 오류 수정

--- src/app/(auth)/mypage/page.tsx
+++ src/app/(auth)/mypage/page.tsx
@@ -3,12 +3,32 @@ import * as S from "@/app/(auth)/mypage/style";
 import Nickname from "@/app/components/mypage/Form/NickName/Nickname";
 import Password from "@/app/components/mypage/Form/Password/Password";
 import UserInfo from "@/app/components/mypage/UserInfo/UserInfo";
+import { useAppSelector } from "@/store/hook";
 import { Wrap } from "@/styles/global";
+import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Mypage() {
 
   const router = useRouter();
+  const user = useAppSelector(state=>state.user);
+
+  useEffect(()=>{
+
+    axios.get('/api/member/mypage',{
+      headers : {
+        Authorization : `${user}`
+      }
+    })
+    .then(({data})=>{
+      console.log(data);
+    })
+    .catch(()=>{
+      console.log('서버 에러');
+    })
+
+  },[user]);
 
   return (
     <S.MypageStyle>
--- src/app/components/auth/Modal/Sign/index.tsx
+++ src/app/components/auth/Modal/Sign/index.tsx
@@ -41,18 +41,19 @@ export default function Sign() {
       password
     })
     .then(({data})=>{
+      console.log(data);
       const {status,message} = data;
       
       if(status === "success"){
         alert('회원가입에 성공하였습니다.');
         return dispatch(onClose());
       }else if(status === "fail"){
-        alert(message);
+        return alert(message);
       }
 
     })
     .catch(err=>{
-      alert('에러가 발생했습니다.');
+      console.log('서버 에러');
     });
 
   }
