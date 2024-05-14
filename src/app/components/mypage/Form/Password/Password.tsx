210bfff5c7679cc299e45bb08f0c189a6f62a9a7
Kim Ji Yu <spbabo97@naver.com>
2024-05-14 16:27:39 +0900

    :sparkles: 비밀번호 수정 기능을 추가했습니다.

--- src/app/components/mypage/Form/Password/Password.tsx
+++ src/app/components/mypage/Form/Password/Password.tsx
@@ -3,26 +3,37 @@ import WhiteBtn from "../../../common/Button/WhiteBtn";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useAppSelector } from "@/store/hook";
+import { useState } from "react";
 
 export default function Password() {
 
     const user = useAppSelector(state=>state.user);
     const {register,handleSubmit} = useForm();
+    const [isError,setIsError] = useState('');
 
     const submithanlder = (event : any)=>{
         const {password,confirmPassword} = event;
 
-        axios.patch('/api/member/mypage',{
-            password,
-            confirmPassword
+        if(password === "" || confirmPassword === ""){
+            return setIsError('비밀번호를 입력하지 않았습니다.');
+        }
+
+        if(password !== confirmPassword) {
+            return setIsError('비밀번호가 동일하지 않습니다.');
+        }
+
+        axios.patch('/api/member/password',{
+            password
         },{
             headers : {
                 Authorization : user
             }
         })
         .then(res=>{
-            const {} = res.data;
-            console.log(res.data);
+            const {status} = res.data;
+            if(status === "success"){
+                alert('비밀번호 변경에 성공하였습니다.');
+            }
         })
         .catch(()=>{
             console.log('서버에러');
@@ -38,18 +49,33 @@ export default function Password() {
                 <S.InputLayout>
                     <label htmlFor="password">새 비밀번호</label>
                     <div>
-                        <input id="password" type="password" placeholder="비밀번호를 입력해주세요." {...register('password')}/>
+                        <input 
+                            id="password" 
+                            type="password" 
+                            placeholder="비밀번호를 입력해주세요." 
+                            {...register('password',{
+                                onChange : ()=> setIsError('')
+                            })}
+                        />
                     </div>
                 </S.InputLayout>
                 <S.InputLayout>
                     <label htmlFor="confirmPassword">새 비밀번호 확인</label>
                     <div>
-                        <input id="confirmPassword" type="password" placeholder="입력한 비밀번호를 입력해주세요." {...register('confirmPassword')}/>
+                        <input 
+                            id="confirmPassword" 
+                            type="password" 
+                            placeholder="입력한 비밀번호를 입력해주세요." 
+                            {...register('confirmPassword',{
+                                onChange : ()=> setIsError('')
+                            })}
+                        />
                     </div>
+                    {isError ? isError : ""}
                 </S.InputLayout>
             </S.InputFlex>
         </S.Layout>
-        <WhiteBtn type="submit" align="center">비밀번호 수정</WhiteBtn>
+        <S.WhiteBtn type="submit">비밀번호 수정</S.WhiteBtn>
     </S.Form>
   )
 }
