299fba82d712a82568deea7bab5a83c54d9dae1d
Kim Ji Yu <spbabo97@naver.com>
2024-06-12 20:24:56 +0900

    :lipstick: 반응형 UI 추가

--- src/app/(public)/find/find.style.ts
+++ src/app/(public)/find/find.style.ts
@@ -1,5 +1,5 @@
 "use client";
-
+import * as I from "@components/common/Input/Basic/Input/Input.style";
 import styled from "styled-components";
 
 export const FindMain = styled.main`
@@ -7,6 +7,7 @@ export const FindMain = styled.main`
 `;
 
 export const FindBox = styled.div`
+    width: 90%;
     max-width: 790px;
     box-sizing: border-box;
     margin: 0 auto;
@@ -38,6 +39,9 @@ export const FindBox = styled.div`
     
     input {
         font-size: ${({theme})=>theme.fontSize.buttonMedium};
+        @media screen and (max-width:820px) {
+            font-size: 14px;
+        }
     }
 
     button {
@@ -47,9 +51,41 @@ export const FindBox = styled.div`
         background-color: ${({theme})=>theme.color.Primary};
         font-size: ${({theme})=>theme.fontSize.buttonMedium};
         border-radius: 8px;
-        margin: 1em auto 0;
+        margin: 16px auto 0;
         display: block;
         cursor: pointer;
+    }
+
+    @media screen and (max-width:820px) {
+
+        padding: 54px 5% 57px;
+        background: none;
+
+        ${I.Box}{
+            height: 50px;
+        }
+
+        form {
+            margin-top: 48px;
+        }
+
+        dl {
+            dt {
+                font-size: 20px;
+            }
+            dd {
+                font-size: 14px;
+                word-break: keep-all;
+            }
+        }
+
+        button {
+            width: 108px;
+            height: 43px;
+            font-size: 16px;
+            margin-top: 19px;
+        }
+
     }
 
 `;
